670ec324b82486136b21b49868eb7fec38a70237
maitrimak <maitrinmakwana9@gmail.com>
2024-07-19 13:32:26 -0400

    Assignment 4 Part 4
    13th Commit- In this I have added all the javascript comments

--- Assignment 4/Part 4/main.js
+++ Assignment 4/Part 4/main.js
@@ -3,21 +3,25 @@ File: assignment4_part4
 Date: 16th July, 2024
 Description: This is the javascript for bouncing balls */
 
-
+// Select the canvas element
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+// Setting the canvas height and width
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+// Function to generate a random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Function to generate a random background colour
 function randomRGB() {
   return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 }
 
+// Shape class to serve as a base for Ball
 class Shape {
   constructor(x, y, velX, velY) {
     this.x = x;
@@ -27,6 +31,7 @@ class Shape {
   }
 }
 
+// extending the ball shape
 class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
@@ -34,12 +39,14 @@ class Ball extends Shape {
     this.size = size;
   }
 
+  // adding a method to draw ball on canvas
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+
 
   update() {
     if ((this.x + this.size) >= width) {
@@ -62,6 +69,7 @@ class Ball extends Shape {
     this.y += this.velY;
   }
 
+  // adding Method to detect collisions 
   collisionDetect() {
     for (const ball of balls) {
       if (this !== ball) {
@@ -77,6 +85,7 @@ class Ball extends Shape {
   }
 }
 
+// Adding EvilCircle class extending Shape,
 class EvilCircle extends Shape {
   constructor(x, y) {
     super(x, y, 20, 20);
@@ -84,6 +93,7 @@ class EvilCircle extends Shape {
     this.size = 10;
   }
 
+  // Adding Method to draw the EvilCircle on the canvas
   draw() {
     ctx.beginPath();
     ctx.lineWidth = 3;
@@ -92,6 +102,7 @@ class EvilCircle extends Shape {
     ctx.stroke();
   }
 
+  // checking the bounds of ball
   checkBounds() {
     if ((this.x + this.size) >= width) {
       this.x = width - this.size;
@@ -110,6 +121,7 @@ class EvilCircle extends Shape {
     }
   }
 
+  // Setting up Method to set up keyboard controls
   setControls() {
     window.onkeydown = (e) => {
       switch (e.key) {
@@ -129,6 +141,7 @@ class EvilCircle extends Shape {
     };
   }
 
+  // Mehtod to detect the collsion
   collisionDetect() {
     for (const ball of balls) {
       const dx = this.x - ball.x;
@@ -143,8 +156,10 @@ class EvilCircle extends Shape {
   }
 }
 
+// empty array to hold all the balls
 const balls = [];
 
+// this will create 25 balls with random properties.
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
@@ -162,16 +177,19 @@ while (balls.length < 25) {
 const evilCircle = new EvilCircle(width / 2, height / 2);
 evilCircle.setControls();
 
+// this is the loop for all the animation.
 function loop() {
   ctx.fillStyle = "rgb(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
+  // Draw, update, and check collisions for balls
   for (const ball of balls) {
     ball.draw();
     ball.update();
     ball.collisionDetect();
   }
 
+  // Draw, update, and check collisions for evil circle
   evilCircle.draw();
   evilCircle.checkBounds();
   evilCircle.collisionDetect();
@@ -179,4 +197,5 @@ function loop() {
   requestAnimationFrame(loop);
 }
 
+// statting animation loop
 loop();
