3238cc3cbf43701ba4f0a974c528e8e4402f8eaf
maitrimak <maitrinmakwana9@gmail.com>
2024-07-19 12:01:04 -0400

    Assignment 4 Part 4
    Sixth Commit- In this I have created a ball class used in the last part and added if blocks for its size and color

--- Assignment 4/Part 4/main.js
+++ Assignment 4/Part 4/main.js
@@ -21,3 +21,39 @@ class Shape {
   }
 }
 
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+ 
